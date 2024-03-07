5bb6b989bf3ab86f03b24b120d693ff44b209504
Maxim Danilov <maxim@wpsoft.at>
2024-03-07 17:03:33 +0100

    update some files

--- tof/__init__.py
+++ tof/__init__.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-from settings import get_language, default_translator
+from os import getenv
+from django.conf import settings, ENVIRONMENT_VARIABLE
+from django.utils import translation
 
-DEFAULT_LANGUAGE = getattr(settings, 'DEFAULT_LANGUAGE', 'en') or 'en'
+default_translator = getattr(translation, 'gettext_lazy' if hasattr(translation, 'gettext_lazy') else 'ugettext_lazy')
+get_language = translation.get_language
+
+DEFAULT_LANGUAGE = getenv(ENVIRONMENT_VARIABLE) and getattr(settings, 'DEFAULT_LANGUAGE', None) or 'en'
