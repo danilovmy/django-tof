187422ca62c3b139a8befaf158dafe8503d0c7e2
Maxim Danilov <maxim@wpshop.at>
2024-03-07 11:20:19 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -40,33 +40,30 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
+# html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = [
-    'custom.css',
-]
-
+# html_css_files = ['custom.css',]
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 add_module_names = False
+htmlhelp_basename = "django-TOF: translations on fly"
 
-
-# set an environment variable for pygeos.decorators.requires_geos to see if we
-# are in a doc build
-import os
-os.environ["SPHINX_DOC_BUILD"] = "1"
+# intersphinx documentation
+intersphinx_mapping = {"tablib": ("https://tablib.readthedocs.io/en/stable/", None)}
