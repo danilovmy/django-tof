9c0b0ae08d705f13c9b4b155270d5af5506e9f1e
Maxim Danilov <maxim@wpsoft.at>
2024-08-04 09:55:19 +0200

    add activate language

--- tof/__init__.py
+++ tof/__init__.py
@@ -1,3 +1,3 @@
-from django.utils.translation import get_language, gettext_lazy as _  #noqa
+from django.utils.translation import activate as activate_language, get_language, gettext_lazy as _  #noqa
 
 LANGUAGES_SOURCE = 'tof.models.Language'
